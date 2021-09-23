81113ce70fd2b6495e6e00bf8a738e52c5f48ac8
angel1021 <90824072+angel1021@users.noreply.github.com>
2021-09-23 15:56:21 +0800

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -6,5 +6,25 @@ Created on Thu Sep 23 15:40:33 2021
 """
 
 a = ['apple','melon', 'orange','pine apple']
-for i in range(3):
-    print(a[i])
+#for i in range(3):
+#    print(a[i])
+    
+b = set(a) 
+#print(b)
+
+b.add('grape')
+#print(b)
+
+b.add('apple')
+#print(b)
+
+b.remove('apple')
+#print(b)
+
+b.discard('apple')
+#print(b)
+
+b.pop()
+#print(b)
+
+set1 = {''}
